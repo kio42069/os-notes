8bac3ff0974fda66a2721fc2434be9864adbe1c5
Surat SS <suratsamanta@gmail.com>
2024-01-01 16:29:17 +0530

    updates

--- run.sh
+++ run.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-
-# Prompt the user for the password
-read -s -p "Enter your Git password: " password
-echo
-
-# Run the Git commands
 git add .
 git commit -m "updates"
-echo $password | git push
-
-# Clear the password variable for security
-unset password
-
+git push
